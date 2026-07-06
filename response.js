4c5e09e957c5a4f96d214e2547e5923292e01556
Bin Xin <rhyzix@gmail.com>
2026-07-06 18:51:44 +0800

    feat: update coord

--- response.js
+++ response.js
@@ -3,12 +3,19 @@ import { patchWithStats, randomPoint } from "./core.js"
 // navigator.geolocation.getCurrentPosition((coords) => console.log(coords), console.error, {enableHighAccuracy:true })
 
 // TODO timer
-const [lat, lng] = randomPoint(30.83556037963936, 120.10123587878017, 20)
+const [lat, lng] = randomPoint(30.3031, 120.10137, 20)
+// const {
+//   results: [{ elevation }],
+// } = await fetch(
+//   `https://api.opentopodata.org/v1/srtm90m?locations=${lat},${lng}`,
+// ).then((res) => res.json())
+const altitude = 8 + Math.random()
+
 patchWithStats(new Uint8Array($response.bodyBytes), {
   latitude: lat,
   longitude: lng,
-  altitude: 8.487911266067622 + Math.random(),
-  accuracy: 19.86234240298098 + Math.random() * 5,
+  altitude,
+  accuracy: 19 + Math.random() * 5,
 })
   .then(({ body, stats }) => {
     console.log(`stats: ${JSON.stringify(stats)}`)
